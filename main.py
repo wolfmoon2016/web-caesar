49cb9e52e4bada4e51bcadb7bc0a507a1c45212b
Lisa Corneau <DLMK831@gmail.com>
2017-01-26 15:20:25 -0500

    wrap text and add button to encrypt message

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ import webapp2
 import caesar
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = ('Hellooooos world!')
-        #encrypted_message = encrypt(message, 13)
-        self.response.write(message)
+        message = 'Hellooooos world!'
+        encrypted_message = caesar.encrypt(message, 13)
+        textarea = "<textarea>"  + encrypted_message + "</textarea>"
+        submit = "<input type= 'submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
